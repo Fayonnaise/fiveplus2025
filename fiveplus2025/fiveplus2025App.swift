3b2bee5c65d07403bd4ef31b53da14a70890949e
fayonnaise <faithannong@gmail.com>
2025-08-14 12:25:12 +0100

    Initial Commit

---
+++ fiveplus2025/fiveplus2025App.swift
@@ -0,0 +1,17 @@
+//
+//  fiveplus2025App.swift
+//  fiveplus2025
+//
+//  Created by Faith Ann Ong on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct fiveplus2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fiveplus2025Tests/fiveplus2025Tests.swift
@@ -0,0 +1,17 @@
+//
+//  fiveplus2025Tests.swift
+//  fiveplus2025Tests
+//
+//  Created by Faith Ann Ong on 14/08/2025.
+//
+
+import Testing
+@testable import fiveplus2025
+
+struct fiveplus2025Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
